ae880426fe126511c56741bb62cfce50cd64cffe
MM <maowei@lrhealth.com>
2025-10-30 14:46:51 +0800

    hbase 的预分区、Phoenix客户端

---
+++ HbaseTest/HbaseTest/src/main/java/com/atguigu/hbase/PhoenixClient.java
@@ -0,0 +1,42 @@
+package com.atguigu.hbase;
+
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * <pre>
+ * +--------+---------+-----------+---------+
+ * |                                        |
+ * +--------+---------+-----------+---------+
+ * </pre>
+ *
+ * @Author Administrator
+ * @Date 2025-10-30 13:39
+ * @Version v2.0
+ */
+public class PhoenixClient {
+
+    public static void main(String[] args) throws SQLException {
+        // 标准的 JDBC 代码
+        // 1.添加链接
+        String url = "jdbc:phoenix:hadoop102,hadoop103,hadoop104:2181";
+        // 2. 创建配置
+        // 没有需要添加的必要配置 因为 Phoenix 没有账号密码
+        Properties properties = new Properties();
+        // 3. 获取连接
+        Connection connection = DriverManager.getConnection(url, properties);
+        // 5.编译 SQL 语句
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from student7");
+        // 6.执行语句
+        ResultSet resultSet = preparedStatement.executeQuery();
+        // 7.输出结果
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(1) + ":" + resultSet.getString(2) + ":" + resultSet.getString(3));
+        }
+        // 8.关闭资源
+        connection.close();
+        // 由于 Phoenix 框架内部需要获取一个 HBase 连接,所以会延迟关闭
+        // 不影响后续的代码执行
+        System.out.println("hello");
+    }
+}
---
+++ HbaseTest/HbaseTest/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * <pre>
+ * +--------+---------+-----------+---------+
+ * |                                        |
+ * +--------+---------+-----------+---------+
+ * </pre>
+ *
+ * @Author Administrator
+ * @Date 2025-10-28 15:39
+ * @Version v2.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
